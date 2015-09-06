132d020f1ccb9a537cf72696cda9ae4c7067d7ed
Prasath Basuvaraj <prasathbasuvaraj@Prasaths-MacBook-Air.local>
2015-09-06 14:16:20 +0530

    Draft Version Published

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/prasathbasuvaraj/Documents/GitWorkSpace/Thirukkural/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/prasathbasuvaraj/Documents/GitWorkSpace/Thirukkural/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/prasathbasuvaraj/Documents/GitWorkSpace/Thirukkural/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/prasathbasuvaraj/Documents/GitWorkSpace/Thirukkural/platforms/android/AndroidManifest.xml \
